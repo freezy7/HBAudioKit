f6566c8ea1f973bdcb42fe99d9150754120b21e0
HolidayBomb <mrducong@qq.com>
2024-06-13 11:59:04 +0800

    update package file

--- Package.swift
+++ Package.swift
@@ -24,7 +24,6 @@ let package = Package (
                 .linkedFramework("Foundation"),
                 .linkedFramework("AVFoundation"),
                 .linkedFramework("AudioToolbox"),
-                .linkedFramework("algorithm"),
                 .linkedFramework("Accelerate"),
             ]
         )
